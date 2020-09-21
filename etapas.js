6ab85f63c14a5efb2f24c58f2140945c2a35f48f
Francisco Josafá <josafa.lopes@gmail.com>
2020-09-20 21:28:16 -0300

    Commit inicia e final do projeto

---
+++ etapas.js
@@ -0,0 +1,58 @@
+let etapas = [
+  {
+    titulo: 'PRESIDENTE',
+    numeros: 2,
+    candidatos: [
+      {
+        numero: '10',
+        nome: 'Aécio Neves',
+        partido: 'ABC',
+        vice: 'José Serra',
+        fotos: [
+          { url: '10.png', legenda: 'Presidente' },
+          { url: '20.png', legenda: 'Vice-Presidente', small: true }
+        ]
+      },
+      {
+        numero: '13',
+        nome: 'Dilma Roussef',
+        partido: 'PT',
+        vice: 'Paulo Maluf',
+        fotos: [
+          { url: '13.png', legenda: 'Presidente' },
+          { url: '40.png', legenda: 'Vice-Presidente', small: true }
+        ]
+      },
+    ]
+  },
+  {
+    titulo: 'SENADOR',
+    numeros: 4,
+    candidatos: [
+      {
+        numero: '2511',
+        nome: 'Marina da Silva',
+        partido: 'PSOL',
+        vice: 'Marta Suplicy',
+        fotos: [
+          { url: '2511.png', legenda: 'Senador', small: false },
+          { url: '3011.png', legenda: 'Vice-Senador', small: true }
+        ]
+      },
+    ]
+  },
+  {
+    titulo: 'VEREADOR',
+    numeros: 5,
+    candidatos: [
+      {
+        numero: '15111',
+        nome: 'Geraldo Alckmin',
+        partido: 'PSDB',
+        fotos: [
+          { url: '15111.png', legenda: 'Vereador' }
+        ]
+      },
+    ]
+  },
+]
